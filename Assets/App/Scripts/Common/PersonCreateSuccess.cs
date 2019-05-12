b850f693d448fcd9b22f868c87a7384c67fb059f
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2019-05-12 22:23:42 +0900

    add model for PersonCreate

---
+++ Assets/App/Scripts/Common/PersonCreateSuccess.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonCreateSuccess : MonoBehaviour
+{
+    [System.Serializable]
+    public class PersonCreateSuccessResponse
+    {
+        public string personId;
+    }
+}
